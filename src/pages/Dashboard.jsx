41d9a80e982bce221984fbaba7ee861bf9f824fd
Bhargav <vyasb1503@gmail.com>
2026-05-14 21:17:22 +0530

    implement the  frontend  dashboard of the  roottrace

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -148,7 +148,7 @@ function Dashboard() {
             <div className="stacktrace-box">
               <h3>Stack Trace</h3>
 
-              <pre>{selectedError.stackTrace}</pre>
+              {/* <pre>{selectedError.stackTrace}</pre> */}
             </div>
           </div>
         </div>
